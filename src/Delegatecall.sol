4b15f859783153eb5be88278672aa614999dc0ee
Vitālijs Gaičuks <vitalijs.gaicuks@gmail.com>
2019-05-01 11:39:33 +0300

    Test contract source

--- src/Delegatecall.sol
+++ src/Delegatecall.sol
@@ -1,4 +1,45 @@
 pragma solidity ^0.5.4;
 
-contract Delegatecall {
+contract DEvents {
+    event LogSender(
+        address sender,
+        uint n
+    );
+
+    event LogResponse(
+        bool success,
+        bytes data
+    );
+}
+
+
+contract Delegatecall is DEvents {
+  uint public n;
+  address public sender;
+
+  function delegateSetN(address _e, uint _n) public {
+    emit LogSender(msg.sender, _n);
+    emit LogSender(sender, n);
+    // D's storage is set, E is not modified
+    (bool success, bytes memory data) = _e.delegatecall(abi.encodePacked(bytes4(keccak256("setN(uint256)")), _n));
+    emit LogResponse(success, data);
+    emit LogSender(sender, n);
+  }
+
+  function callSetN(address _e, uint _n) public {
+    emit LogSender(msg.sender, _n);
+    (bool success, bytes memory data) = _e.call(abi.encodeWithSignature("setN(uint)", _n));
+    emit LogResponse(success, data);
+  }
+
+}
+
+contract E {
+  uint public n;
+  address public sender;
+
+  function setN(uint _n) public {
+    n = _n;
+    sender = msg.sender;
+  }
 }
--- src/Delegatecall.t.sol
+++ src/Delegatecall.t.sol
@@ -6,9 +6,11 @@ import "./Delegatecall.sol";
 
 contract DelegatecallTest is DSTest {
     Delegatecall delegatecall;
+    E e;
 
     function setUp() public {
         delegatecall = new Delegatecall();
+        e = new E();
     }
 
     function testFail_basic_sanity() public {
@@ -17,5 +19,13 @@ contract DelegatecallTest is DSTest {
 
     function test_basic_sanity() public {
         assertTrue(true);
+    }
+
+    function test_call() public {
+        delegatecall.callSetN(address(e), 1 ether);
+    }
+
+    function test_delegatecall() public {
+        delegatecall.delegateSetN(address(e), 1 ether);
     }
 }
